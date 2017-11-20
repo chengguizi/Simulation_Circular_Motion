2d6ae0f43c4d3a4055494785cc430a7d3db7188e
chengguizi <cgz_05@sina.com>
2017-11-21 00:08:32 +0800

    Fix plot errors in V-correct figures

--- correct_with_velocity_figure.m
+++ correct_with_velocity_figure.m
@@ -21,15 +21,15 @@ legend('EKF Input A_x','Ground Truth A_x')
 ylim([-1.5 1.5]);
 
 subplot(313)
-plot(truth(:,1),knoise1(:,1));
+plot(truth(:,1),knoise2(:,1));
 hold on
-plot(truth(:,1),knoise1(:,3));
+plot(truth(:,1),knoise2(:,3));
 title('[V-Correct] K gain of EKF');
 legend('K of Position','K of Velocity');
     
 
 figure
-plot(x_p_corrected(:,1),x_v_corrected(:,2));
+plot(x_v_corrected(:,1),x_v_corrected(:,2));
 hold on
 plot(x_naive(:,1),x_naive(:,2));
 plot(truth(:,2),truth(:,3),'--');
@@ -37,5 +37,8 @@ title('[V-Correct] Output Position');
 legend('Full EKF','EKF Prediction Only','Ground Truth')
 grid
 
-% figure
-% plot(x_p_corrected(:,3),x_v_corrected(:,4));
+ figure
+ plot(truth(:,1),x_v_corrected(:,1));
+ hold on
+ plot(truth(:,1),truth(:,2));
+ title('[V-Correct] Output Position P_x');
